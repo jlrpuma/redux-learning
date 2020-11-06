86df67b0b26fd7ee80f2658f030b230cc473de45
Jose Luis Rodriguez <jose@Joses-Mac-mini.local>
2020-11-05 23:13:47 -0500

    Small code showing the concept of functional programming

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
-console.log("Hello World!");
+// normal approach
+let input = "   JavaScript   ";
+let output = "<div>" + input + "</div>";
+
+
+// functional approach
+const trim = str => str.trim();
+const wrapInDiv = str => `<div>${str}</div>`;
+
+// composition of functions to achieve the same result 
+/*
+
+But this allow us to reuse the function trim or wrap at any place of the code
+
+*/
+const result = wrapInDiv(trim(input));
